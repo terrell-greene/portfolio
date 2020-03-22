2905fff3e8c4c8b6ce476e9223551324389e113d
terrell-greene <33211911+terrell-greene@users.noreply.github.com>
2020-03-21 23:35:34 -0500

    Updated static files

--- lib/portfolio_web/views/layout_view.ex
+++ lib/portfolio_web/views/layout_view.ex
@@ -1,3 +1,31 @@
 defmodule PortfolioWeb.LayoutView do
   use PortfolioWeb, :view
+
+  def nav_link(conn, path, text) do
+    current_path = Path.join(["/" | conn.path_info])
+    active? = path == current_path
+
+    list_styles = active_link_class?(active?)
+
+    content_tag :li, class: list_styles do
+      link(to: path, class: "nav-link") do
+        active_link_content?(active?, text)
+      end
+    end
+  end
+
+  defp active_link_class?(active?) do
+    if active?, do: "nav-item active", else: "nav-item"
+  end
+
+  defp active_link_content?(active?, text) do
+    if active? do
+      [
+        text,
+        content_tag(:span, "(current)", class: "sr-only")
+      ]
+    else
+      text
+    end
+  end
 end
