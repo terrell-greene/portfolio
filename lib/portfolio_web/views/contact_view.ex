f2e929be5d125b98d8bc6619aff9e098725f7736
terrell-greene <33211911+terrell-greene@users.noreply.github.com>
2020-03-25 17:52:33 -0500

    Added contact view

--- lib/portfolio_web/views/contact_view.ex
+++ lib/portfolio_web/views/contact_view.ex
@@ -1,3 +1,10 @@
 defmodule PortfolioWeb.ContactView do
   use PortfolioWeb, :view
+
+  def render("scripts.index.html", _assigns) do
+    ~E"""
+      <script src="/plugins/jquery-validation/jquery.validate.min.js"></script>
+      <script src="/js/form.js"></script>
+    """
+  end
 end
